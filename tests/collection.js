cc8bd5121dd75409bde79ccc9b0aea9b887bee98
ahadsiddiqui <ahadsiddiqui094@gmail.com>
2025-05-27 14:55:41 +1000

    Create collection.js

---
+++ tests/collection.js
@@ -0,0 +1,27 @@
+{
+  "info": {
+    "name": "Goof API Tests",
+    "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
+  },
+  "item": [
+    {
+      "name": "Login Test",
+      "request": {
+        "method": "POST",
+        "header": [
+          { "key": "Content-Type", "value": "application/json" }
+        ],
+        "body": {
+          "mode": "raw",
+          "raw": "{ \"username\": \"testuser\", \"password\": \"123456\" }"
+        },
+        "url": {
+          "raw": "{{base_url}}/login",
+          "host": ["{{base_url}}"],
+          "path": ["login"]
+        }
+      },
+      "response": []
+    }
+  ]
+}
